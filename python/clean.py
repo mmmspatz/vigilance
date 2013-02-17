c0c5848733688ad928166a0e662bcad301e6b6c6
Pranjal Vachaspati <pranjal@mit.edu>
2013-02-17 16:05:56 -0500

    Added script to remove old images and device we haven't heard from recently

---
+++ python/clean.py
@@ -0,0 +1,25 @@
+import sqlite3
+from config import *
+import os
+import datetime
+
+def clean_files():
+    tm = datetime.datetime.now() - image_lifetime
+    conn = sqlite3.connect(dbname)    
+    oldfiles = conn.execute('SELECT * FROM files WHERE filetime < ?', (tm,)).fetchmany()
+    for (devid, fname, ftime) in oldfiles:
+        os.remove(fname)
+    conn.execute('DELETE FROM files WHERE filetime < ?', (tm,))
+    conn.commit()
+    conn.close()
+    
+def clean_devices():
+    tm = datetime.datetime.now() - device_lifetime
+    conn = sqlite3.connect(dbname)    
+    conn.execute('DELETE FROM devices WHERE ping < ?', (tm,))
+    conn.commit()
+    conn.close()    
+
+if __name__ == "__main__":
+    clean_files()
+    clean_devices()
